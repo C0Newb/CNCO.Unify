410e8249dde54a57c915d378c9f6eca647208c4d
C0Newb <cody@cnewb.co>
2025-09-16 22:19:03 -0400

    Update packages

--- tests/UnifyTestRunner/UnifyTestRunner.Browser/Program.cs
+++ tests/UnifyTestRunner/UnifyTestRunner.Browser/Program.cs
@@ -6,13 +6,13 @@ using System.Runtime.Versioning;
 using System.Threading.Tasks;
 
 [assembly: SupportedOSPlatform("browser")]
-
-internal partial class Program {
-    private static async Task Main(string[] args) => await BuildAvaloniaApp()
-            .WithInterFont()
-            .UseReactiveUI()
-            .StartBrowserAppAsync("out");
-
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>();
+namespace UnifyTestRunner.Browser {
+    public class Program {
+        public static async Task Main(string[] args) {
+            await AppBuilder.Configure<App>()
+                .WithInterFont()
+                .UseReactiveUI()
+                .StartBrowserAppAsync("out");
+        }
+    }
 }
